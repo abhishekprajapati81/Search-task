cb261a43e4787e8a6a2e1443a8432cfcf0efb26e
abhishekprajapati81 <abhiprajapatii8128@gmail.com>
2023-12-08 19:02:27 +0530

    today search task using rxjs

--- src/app/person/person/person.component.ts
+++ src/app/person/person/person.component.ts
@@ -8,6 +8,7 @@ import {
   delay,
   distinctUntilChanged,
   map,
+  startWith,
 } from 'rxjs/operators';
 import { FormControl, Validators } from '@angular/forms';
 
@@ -20,76 +21,32 @@ export class PersonComponent implements OnInit {
   users$!: Observable<User[]>;
   filteredUsers$!: Observable<User[]>;
   searchFrom = new FormControl('', Validators.required);
-  searchTerm$!: Observable<string | null>;
-  temp = '';
-  priviousData: any;
-  // temp!: string;
-
+  previousSearch = '';
   constructor(private userService: UsersService) {
     this.users$ = this.userService.all();
   }
 
   ngOnInit() {
-    this.users$.subscribe((res) => {
-      console.log(res);
-    });
-    this.filteredUsers$ = this.users$;
-    this.searchTerm$ = this.searchFrom.valueChanges;
-    this.searchTerm$.subscribe((data) => {
-      console.log('', data);
-    });
-  }
-
-  //   searchUser() {
-  //     this.temp = this.searchFrom.value ?? '';
-  //     console.log(this.temp);
-  //     if (this.temp.length >= 3) {
-  //       this.filteredUsers$ = this.users$.pipe(
-  //         map((users) =>
-  //           users.filter(
-  //             (user) =>
-  //               user.firstName.toLowerCase().includes(this.temp.toLowerCase()) ||
-  //               user.lastName.toLowerCase().includes(this.temp.toLowerCase())
-  //           )
-  //         ),
-  //         delay(500)
-  //       );
-  //     } else {
-  //       this.filteredUsers$ = this.users$;
-  //     }
-  //   }
-  // }
-
-  searchUser() {
-    const cuurentsearchString = this.searchFrom.value
-      ? this.searchFrom.value.toLowerCase()
-      : '';
-    this.priviousData = this.users$;
-    console.log('privious data', this.priviousData);
-
-    if (
-      cuurentsearchString &&
-      cuurentsearchString.length >= 3 &&
-      cuurentsearchString !== this.priviousData
-    ) {
-      this.dataLoadFunction();
-    } else {
-      this.filteredUsers$ = this.users$;
-    }
-    this.temp = cuurentsearchString;
-  }
-
-  dataLoadFunction() {
-    this.filteredUsers$ = this.users$.pipe(
-      map((users) =>
-        users.filter(
-          (user) =>
-            user.firstName.toLowerCase().includes(this.temp.toLowerCase()) ||
-            user.lastName.toLowerCase().includes(this.temp.toLowerCase())
-        )
-      ),
-      delay(1500)
-    );
-    console.log(this.filteredUsers$);
+    this.searchFrom.valueChanges
+      .pipe(startWith(''), debounceTime(1300), distinctUntilChanged())
+      .subscribe((searchString) => {
+        if (searchString && searchString.length >= 3) {
+          this.filteredUsers$ = this.users$.pipe(
+            map((users) =>
+              users.filter(
+                (user) =>
+                  user.firstName
+                    .toLowerCase()
+                    .includes(searchString.toLowerCase()) ||
+                  user.lastName
+                    .toLowerCase()
+                    .includes(searchString.toLowerCase())
+              )
+            )
+          );
+        } else if (!searchString || searchString.length < 3) {
+          this.filteredUsers$ = this.users$;
+        }
+      });
   }
 }
